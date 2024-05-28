65d6995d7d2913206884dd20c7a7229248177a7f
soyo <166856576+soyoki@users.noreply.github.com>
2024-05-28 17:30:46 -0300

    input.py

--- input.py
+++ input.py
@@ -1,1 +1,4 @@
+nome = input("Digite o seu nome: ") # Criei uma variável escrito nome com uma barra para a pessoa escrever
+
+print("Olá,", nome, "! Bem-vindo!") # Crio um print para mostrar na tela saudações ao nome que o usuário colocou, até coloco um ! para juntar com o nome variável e com o bem-vindo.
 
